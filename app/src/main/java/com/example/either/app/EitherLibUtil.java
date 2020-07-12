839f41eb6801abd290f682d052e0646c170f3b0b
TonyTandAndroid <tonythompsoncmu@gmail.com>
2020-07-12 16:08:31 -0700

    Make it to gradle build library to test jitpack support

---
+++ app/src/main/java/com/example/either/app/EitherLibUtil.java
@@ -0,0 +1,19 @@
+package com.example.either.app;
+
+import com.spencerwi.either.Either;
+
+public class EitherLibUtil {
+
+  public static Either<Throwable, Boolean> build(boolean flag) {
+    return flag ? Either.right(true) : Either.left(new RuntimeException("exception"));
+  }
+
+  /**
+   * A java 8 method reference in library without enable desugar passes compile.
+   * If the app is not aware of such assumption and did not enable the desugar flag, it will crash.
+   */
+  public static String foldBelow23(Either<Throwable, Boolean> either) {
+    Either<String, String> map = either.map(Throwable::getMessage, String::valueOf);
+    return map.isLeft() ? map.getLeft() : map.getRight();
+  }
+}
---
+++ app/src/main/java/com/example/either/app/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.either.app;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+
+  public void click(View view) {
+    view.postDelayed(this::showToast, 300);
+  }
+
+  private void showToast() {
+    show(EitherUtil.fold(EitherUtil.either()));
+  }
+
+  private void show(String message) {
+    Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+  }
+}
